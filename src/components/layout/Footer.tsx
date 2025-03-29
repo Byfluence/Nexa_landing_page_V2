ac6fe59b817146e4651ab000b242b0eb27f01785
eyasu tilahun <49980352+eyotile@users.noreply.github.com>
2025-03-29 18:19:50 +0300

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -72,7 +72,7 @@ const Footer = () => {
                 </a>
               </li>
               <li>
-                <a href="tel:+251 993 95 4037" className="flex items-center text-gray-400 hover:text-white transition-colors">
+                <a href="tel:+251 932 00 5810" className="flex items-center text-gray-400 hover:text-white transition-colors">
                   <Phone className="w-5 h-5 mr-2" />
                   +251 932 00 5810
                 </a>
